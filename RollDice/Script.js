b44e046a2f8ad25b36a638c4021b1fe154e90233
Nayan RajendraPrasad <nayan.r2020@gmail.com>
2024-03-29 12:26:41 +0530

    Added mp3 files

--- RollDice/Script.js
+++ RollDice/Script.js
@@ -7,38 +7,49 @@ function convert(){
 let val=Math.floor(Math.random()*6) +1;
 val = Number(val);
 var audio = document. getElementById("sound"); 
-audio. play();
 
 switch(val){
         case 1:
             images.innerHTML=`<img src="${val}.png" width="100px">`;
+            audio.innerHTML=`<audio controls autoplay><source src="${val}.mp3" type="audio/mpeg"></audio>`
             dice.style.display="none";
             shownum.textContent="1";
+            audio. play();
             break;
         case 2:
             images.innerHTML=`<img src='${val}.png' width="100px">`;
+            audio.innerHTML=`<audio controls autoplay><source src="${val}.mp3" type="audio/mpeg"></audio>`
             dice.style.display="none";
             shownum.textContent="2";
+            audio. play();
             break;
         case 3:
             images.innerHTML=`<img src='${val}.png' width="100px">`;
+            audio.innerHTML=`<audio controls autoplay><source src="${val}.mp3" type="audio/mpeg"></audio>`
             dice.style.display="none";
             shownum.textContent="3";
+            audio. play();
             break;
         case 4:
             images.innerHTML=`<img src='${val}.png' width="100px">`;
+            audio.innerHTML=`<audio controls autoplay><source src="${val}.mp3" type="audio/mpeg"></audio>`
             dice.style.display="none";
             shownum.textContent="4";
+            audio. play();
             break;
         case 5:
             images.innerHTML=`<img src='${val}.png' width="100px">`;
+            audio.innerHTML=`<audio controls autoplay><source src="${val}.mp3" type="audio/mpeg"></audio>`
             dice.style.display="none";
             shownum.textContent="5";
+            audio. play();
             break;
         case 6:
             images.innerHTML=`<img src='${val}.png' width="100px">`;
+            audio.innerHTML=`<audio controls autoplay><source src="${val}.mp3" type="audio/mpeg"></audio>`
             dice.style.display="none";
             shownum.textContent="6";
+            audio. play();
             break;
            
     }
